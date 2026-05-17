a8053973b9c786b8316df3db1534ff470b77cc80
strangecodee <anuragmaurya883@gmail.com>
2026-05-17 17:34:45 +0530

    update owasp agin

--- vars/runDependencyCheck.groovy
+++ vars/runDependencyCheck.groovy
@@ -2,13 +2,17 @@ def call() {
 
     echo "Running OWASP Dependency Check"
 
-    dependencyCheck additionalArguments: '''
-        --scan .
-        --format HTML
-    ''',
-    odcInstallation: 'OWASP-Dependency-Check'
+    dependencyCheck(
+        additionalArguments: '''
+            --scan .
+            --format HTML
+        ''',
+        odcInstallation: 'OWASP-Dependency-Check'
+    )
 
-    dependencyCheckPublisher pattern: 'dependency-check-report.xml'
+    dependencyCheckPublisher(
+        pattern: 'dependency-check-report.xml'
+    )
 
-    echo "OWASP Dependency Check completed"
+    echo "OWASP Dependency Check completed successfully"
 }
